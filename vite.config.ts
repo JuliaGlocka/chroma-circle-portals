3d38765a4cea7a47d862fc1d7bfb3f644663716f
glock <glockajulia@gmail.com>
2025-07-29 16:59:28 +0200

    redactor code

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/devjuliaglocka/",
+    base: mode === 'production' ? '/devjuliaglocka/' : '/',
 
   server: {
     host: "::",
